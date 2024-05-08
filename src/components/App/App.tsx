8450fc1042c66859592b92b25eca040270343e64
dmvoytyuk <dmvoytyuk@gmail.com>
2024-05-08 12:09:15 +0200

    typing Api response

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,90 +8,95 @@ import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
 import ImageModal from "../ImageModal/ImageModal";
 import { Image } from "../../types/types.ts";
 
+export type ApiResponse = {
+	results: Image[];
+	total_pages: number;
+};
+
 function App() {
-  const [images, setImages] = useState<Image[]>([]);
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalImage, setModalImage] = useState<Image | undefined>(undefined);
-  const [query, setQuery] = useState<string | null>(null);
-  const loadMoreBtnRef = useRef<HTMLButtonElement | null>(null);
+	const [images, setImages] = useState<Image[]>([]);
+	const [page, setPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(1);
+	const [isError, setIsError] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [modalImage, setModalImage] = useState<Image | undefined>(undefined);
+	const [query, setQuery] = useState<string | null>(null);
+	const loadMoreBtnRef = useRef<HTMLButtonElement | null>(null);
 
-  useEffect(() => {
-    if (query) {
-      const fetchImages = async () => {
-        try {
-          setIsLoading(true);
-          const fetchedImages = await getImages(page, query);
-          setImages((currImages) => {
-            return [...currImages, ...fetchedImages.results];
-          });
-          setTotalPages(fetchedImages.total_pages);
-        } catch (error) {
-          setIsError(true);
-        } finally {
-          setIsLoading(false);
-        }
-      };
-      fetchImages();
-    }
-  }, [page, query]);
+	useEffect(() => {
+		if (query) {
+			const fetchImages = async () => {
+				try {
+					setIsLoading(true);
+					const fetchedImages = (await getImages(page, query)) as ApiResponse;
+					setImages((currImages) => {
+						return [...currImages, ...fetchedImages.results];
+					});
+					setTotalPages(fetchedImages.total_pages);
+				} catch (error) {
+					setIsError(true);
+				} finally {
+					setIsLoading(false);
+				}
+			};
+			fetchImages();
+		}
+	}, [page, query]);
 
-  const searchImages = (newQuery: string) => {
-    if (query !== newQuery) {
-      setQuery(newQuery);
-      setPage(1);
-      setImages([]);
-    }
-  };
+	const searchImages = (newQuery: string) => {
+		if (query !== newQuery) {
+			setQuery(newQuery);
+			setPage(1);
+			setImages([]);
+		}
+	};
 
-  const loadMore = () => {
-    setPage((currPage) => currPage + 1);
-  };
+	const loadMore = () => {
+		setPage((currPage) => currPage + 1);
+	};
 
-  const openModal = (imageId: string) => {
-    setIsModalOpen(true);
-    const imageToShow = images.find((image) => imageId === image.id);
-    setModalImage(imageToShow);
-  };
+	const openModal = (imageId: string) => {
+		setIsModalOpen(true);
+		const imageToShow = images.find((image) => imageId === image.id);
+		setModalImage(imageToShow);
+	};
 
-  const closeModal = () => {
-    document.body.classList.remove("ReactModal__Body--open");
-    setIsModalOpen(false);
-  };
+	const closeModal = () => {
+		document.body.classList.remove("ReactModal__Body--open");
+		setIsModalOpen(false);
+	};
 
-  useEffect(() => {
-    if (loadMoreBtnRef.current !== null) {
-      loadMoreBtnRef.current.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "start",
-      });
-    }
-  }, [isLoading]);
+	useEffect(() => {
+		if (loadMoreBtnRef.current !== null) {
+			loadMoreBtnRef.current.scrollIntoView({
+				behavior: "smooth",
+				block: "center",
+				inline: "start",
+			});
+		}
+	}, [isLoading]);
 
-  return (
-    <>
-      <SearchBar searchImages={searchImages} />
-      {images.length > 0 && (
-        <ImageGallery images={images} openModal={openModal} />
-      )}
-      {page < totalPages && !isLoading && (
-        <LoadMoreBtn ref={loadMoreBtnRef} loadMore={loadMore} />
-      )}
-      {isLoading && <Loader />}
-      {isError && <ErrorMessage />}
-      {isModalOpen && modalImage && (
-        <ImageModal
-          isModalOpen={isModalOpen}
-          closeModal={closeModal}
-          image={modalImage}
-        />
-      )}
-    </>
-  );
+	return (
+		<>
+			<SearchBar searchImages={searchImages} />
+			{images.length > 0 && (
+				<ImageGallery images={images} openModal={openModal} />
+			)}
+			{page < totalPages && !isLoading && (
+				<LoadMoreBtn ref={loadMoreBtnRef} loadMore={loadMore} />
+			)}
+			{isLoading && <Loader />}
+			{isError && <ErrorMessage />}
+			{isModalOpen && modalImage && (
+				<ImageModal
+					isModalOpen={isModalOpen}
+					closeModal={closeModal}
+					image={modalImage}
+				/>
+			)}
+		</>
+	);
 }
 
 export default App;
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,17 +1,16 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.unsplash.com/",
+	baseURL: "https://api.unsplash.com/",
 });
 
 axiosInstance.defaults.headers.common["Authorization"] =
-  "Client-ID cP1i_US6CxcYojs6I04AtV8BC1JoXhBCzAIWy90xAek";
+	"Client-ID cP1i_US6CxcYojs6I04AtV8BC1JoXhBCzAIWy90xAek";
 axiosInstance.defaults.headers.common["Accept"] = "v1";
 
-export const getImages = async (page: number, query: string) => {
-  const images = await axiosInstance.get(
-    `/search/photos?page=${page}&query=${query}`,
-  );
-  console.log(images.data);
-  return images.data;
+export const getImages = async <T>(page: number, query: string): Promise<T> => {
+	const images = await axiosInstance.get(
+		`/search/photos?page=${page}&query=${query}`,
+	);
+	return images.data;
 };
